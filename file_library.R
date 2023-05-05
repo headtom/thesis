be5b529f04c95c838f72dfcc69ab9175eec21ec7
Thomas Head <thomasmhead@gmail.com>
2023-05-04 19:02:25 -0700

    .csv files and R workspace

---
+++ file_library.R
@@ -0,0 +1,21 @@
+#### General Files ####
+"names_list.RData"
+"mrn_points.RData"
+"full_ppm_vectors.RData"
+
+#### Name List Files ####
+"ordered_df_names.rds"
+"full_df_names.rds"
+"full_ppm_vectors.rds"
+"full_lineplot_data_names.rds"
+
+#### data_processing Files ####
+"reformatted_raw_data.RData"
+"full_data.RData"
+"full_lineplot_data.RData"
+
+#### Exploratory Observation Files ####
+"full_ppm_info.rds"
+
+#### Visualization Files ####
+"full_lineplots.RData"
